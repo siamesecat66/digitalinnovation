28f167662f24ec1c3b83dfb443b9a1dea551dd81
siamesecat66 <reinaldoybk@hotmail.com>
2021-08-19 00:21:38 -0300

    class members

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,14 +2,15 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Siamese"
-
     var cpf: String = "123.123.123.99"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main() {
     val siamese = Pessoa()
-
-    println(siamese.nome)
-    println(siamese.cpf)
+    println(siamese.pessoaInfo())
 }
