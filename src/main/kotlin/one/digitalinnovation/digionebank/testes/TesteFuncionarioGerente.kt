74d8a0e67711d3f82aa77769ad906af16c8f48a3
siamesecat66 <reinaldoybk@hotmail.com>
2021-08-19 06:30:12 -0300

    created Gerente and Analista class, new tests

--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -3,6 +3,7 @@ package one.digitalinnovation.digionebank.testes
 import one.digitalinnovation.digionebank.Gerente
 
 fun main() {
-    val maria = Gerente("Maria do Carmo", "123.123.123.99", 5000.0)
+    val maria = Gerente("Maria do Carmo", "123.123.123.99", 5000.0, "123")
     ImprimeRelatorioFuncionario.imprime(maria)
+    TesteAutenticacao().autentica(maria)
 }
